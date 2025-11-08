02ccff0bbf994cf82c3087503fe0e0d825d60367
dumpling815 <ericminuk@gmail.com>
2025-11-08 23:18:27 +0900

    <update> update logger level DEBUG ->INFO

--- data_pulling/pdf_analysis.py
+++ data_pulling/pdf_analysis.py
@@ -9,7 +9,7 @@ from ollama import chat, ChatResponse, Options
 
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 
 def markdownize_tables(tables: list[AssetTable]) -> str:
