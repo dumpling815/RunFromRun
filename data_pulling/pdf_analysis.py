9c9be4c7f2ca4e034b0f3daf3569d0fdc117ad6b
dumpling815 <ericminuk@gmail.com>
2025-11-09 22:54:59 +0900

    <feat> added test case for all pdf

--- data_pulling/pdf_analysis.py
+++ data_pulling/pdf_analysis.py
@@ -246,3 +246,19 @@ if __name__ == "__main__":
     pdf_path = Path("./test/report/USDC.pdf") # [DEBUG] 테스트용 PDF 경로
     result_table = analyze_pdf_local_llm(pdf_path, stablecoin="USDC")
     plotit_asset_tables(stablecoin="USDC", asset_table=result_table)
+
+    pdf_path = Path("./test/report/FDUSD.pdf") # [DEBUG] 테스트용 PDF 경로
+    result_table = analyze_pdf_local_llm(pdf_path, stablecoin="FDUSD")
+    plotit_asset_tables(stablecoin="FDUSD", asset_table=result_table)
+
+    pdf_path = Path("./test/report/PYUSD.pdf") # [DEBUG] 테스트용 PDF 경로
+    result_table = analyze_pdf_local_llm(pdf_path, stablecoin="PYUSD")
+    plotit_asset_tables(stablecoin="PYUSD", asset_table=result_table)
+
+    pdf_path = Path("./test/report/TUSD.pdf") # [DEBUG] 테스트용 PDF 경로
+    result_table = analyze_pdf_local_llm(pdf_path, stablecoin="TUSD")
+    plotit_asset_tables(stablecoin="TUSD", asset_table=result_table)
+
+    pdf_path = Path("./test/report/USDP.pdf") # [DEBUG] 테스트용 PDF 경로
+    result_table = analyze_pdf_local_llm(pdf_path, stablecoin="USDP")
+    plotit_asset_tables(stablecoin="USDP", asset_table=result_table)
