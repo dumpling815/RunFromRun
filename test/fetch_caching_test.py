a0fc8cb7f1c171070b51db9788ff20b4a2005d1f
dumpling815 <ericminuk@gmail.com>
2025-11-14 16:16:11 +0900

    <fix> fixed fetch_caching_test.py -> added await

--- test/fetch_caching_test.py
+++ test/fetch_caching_test.py
@@ -1,8 +1,13 @@
-if __name__ == "__main__":
+import asyncio
+
+async def main():
     from data_pulling.pdf_analysis import analyze_pdf, plotit_asset_tables
     id = 1
     report_pdf_url = "https://assets.ctfassets.net/vyse88cgwfbl/6GbUTVK4tTYAytefu5daIi/6cac18eb4b526c9c52640a3d2bed9642/ISAE_3000R_-_Opinion_Tether_International_Financial_Figure_31-10-2025.pdf"
     stablecoin = "USDT"
     asset_table = await analyze_pdf(id=id,report_pdf_url=report_pdf_url,stablecoin=stablecoin)
     plotit_asset_tables(stablecoin="USDT", asset_table=asset_table)
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
