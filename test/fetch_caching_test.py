7572aed63a225f0177e7dd6be6497756ad8c853c
dumpling815 <ericminuk@gmail.com>
2025-11-14 16:26:27 +0900

    <fix> fixed test file: id should be string

--- test/fetch_caching_test.py
+++ test/fetch_caching_test.py
@@ -2,7 +2,7 @@ import asyncio
 
 async def main():
     from data_pulling.pdf_analysis import analyze_pdf, plotit_asset_tables
-    id = 1
+    id = "1"
     report_pdf_url = "https://assets.ctfassets.net/vyse88cgwfbl/6GbUTVK4tTYAytefu5daIi/6cac18eb4b526c9c52640a3d2bed9642/ISAE_3000R_-_Opinion_Tether_International_Financial_Figure_31-10-2025.pdf"
     stablecoin = "USDT"
     asset_table = await analyze_pdf(id=id,report_pdf_url=report_pdf_url,stablecoin=stablecoin)
